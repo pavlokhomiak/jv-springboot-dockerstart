51e19202374e226afe6e38bf4093c485366c9113
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2022-06-01 16:59:24 +0300

    first commit

---
+++ src/main/java/com/example/jvspringbootdockerstart/JvSpringbootDockerstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.jvspringbootdockerstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvSpringbootDockerstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvSpringbootDockerstartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jvspringbootdockerstart/JvSpringbootDockerstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jvspringbootdockerstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvSpringbootDockerstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
